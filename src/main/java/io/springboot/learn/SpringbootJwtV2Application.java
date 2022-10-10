bf141150747451aaaf29cfb97c843aaa29a26776
priyo das <priyodas12@gmail.com>
2022-10-10 21:40:58 +0530

    Initial commit

---
+++ src/main/java/io/springboot/learn/SpringbootJwtV2Application.java
@@ -0,0 +1,13 @@
+package io.springboot.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/springboot/learn/SpringbootJwtV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
